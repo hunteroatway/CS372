6cc77c47496ced324d55552980433d8eb25938ed
Hamza-Imtiaz <63941147+Hamza-Imtiaz@users.noreply.github.com>
2020-11-20 22:07:44 -0600

    Create profile.php

---
+++ pages/profile.php
@@ -0,0 +1,93 @@
+<?php
+
+session_start();
+if(($_SESSION['email'] == true && $_SESSION['password'] == true)){
+	true;
+}else{
+	echo ("<script LANGUAGE='JavaScript'>
+    window.alert('You need to LogIn in order to access on this page.');
+    window.location.href='index.php';
+    </script>");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel = "stylesheet"
+              type = "text/css"
+              href = "../css/myStyle.css" />
+        <title>Profile page</title> 
+                <style>
+            .err_msg{ color:red;}
+        </style>
+    </head>
+
+    <header>
+	<img src="../images/logo.png" style = "display:inline" width = "250" height = "200" />
+    </header>
+
+    <body>
+         <div class="topnav">
+            <a class="active" href="index.html">Home <i class="fa fa-fw fa-home"> </i></a>
+            <a href="signUp.html">SignUp <i class="fa fa-user"> </i></a>
+            <a href=".html">Manage</a>
+            <a href=".html">Book <i class="fa fa-book"> </i></a>
+			  <div class="search-container">
+				<form action="/action_page.php">
+                <input type="text" placeholder="City.." name="city">
+				<input type="text" placeholder="Search.." name="search">
+				<button type="submit"><i class="fa fa-search"></i></button>
+				</form>
+			</div>
+        </div>
+<br>
+        <img class="pic" src="../images/avatar.gif" alt="profile" style = "display:inline" width = "100" height = "100" />
+        <p>UserID: hello@34</p>
+		<hr/>
+        <h1>Most Recent Posting</h1>
+        <div class="result">
+
+            <div class="post">
+                <img class="bookImage" src="../images/book_placeholder.jpg" width="200" height="200" alt="Book Image"/>
+                <p>Book Title</p>
+                <p>Book Author</p>
+                <p>ISBN</p>
+                <p>Price</p>
+                <p>Location</p>
+            </div>
+
+            <div class="post">
+                <img class="bookImage" src="../images/book_placeholder.jpg" width="200" height="200" alt="Book Image"/>
+                <p>Book Title</p>
+                <p>Book Author</p>
+                <p>ISBN</p>
+                <p>Price</p>
+                <p>Location</p>
+            </div>
+
+            <div class="post">
+                <img class="bookImage" src="../images/book_placeholder.jpg" width="200" height="200" alt="Book Image"/>
+                <p>Book Title</p>
+                <p>Book Author</p>
+                <p>ISBN</p>
+                <p>Price</p>
+                <p>Location</p>
+            </div>
+
+            <div class="post">
+                <img class="bookImage" src="../images/book_placeholder.jpg" width="200" height="200" alt="Book Image"/>
+                <p>Book Title</p>
+                <p>Book Author</p>
+                <p>ISBN</p>
+                <p>Price</p>
+                <p>Location</p>
+            </div>
+            
+        </div>
+
+              <script type="text/javascript" src="../js/JavaScript.js"></script>
+    </body>
+</html>
