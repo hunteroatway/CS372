1afef4373fb9c2265efbf06006a7edbdee6549ea
hunteroatway <oatway.h@gmail.com>
2020-11-26 19:24:44 -0600

    readded subtitle field, can pull from google books api into fields value and handle undefined values for fields

--- js/search.js
+++ js/search.js
@@ -1,18 +1,21 @@
 // Changes input fields based on toggle switch input for automatic or manual data entry
 function selectPostingType(event) {
 	var title = document.getElementById("title");
+	var subtitle = document.getElementById("subtitle");
 	var author = document.getElementById("author");
 	var description = document.getElementById("description");
 	var publisher = document.getElementById("publisher");
 
 	// Toggle readonly attribute for each field
 	toggleReadOnly(title);
+	toggleReadOnly(subtitle);
 	toggleReadOnly(author);	
 	toggleReadOnly(description);
 	toggleReadOnly(publisher);
 
 	// Reset placeholder values
 	title.value = '';
+	subtitle.value = '';
 	author.value = '';
 	description.value = '';
 	publisher.value = '';
@@ -39,20 +42,30 @@ function searchBookByISBN() {
         console.log("Book Content from Google Books API");
         console.log(obj);          
 
-        var title = obj.items[0].volumeInfo.title;
-        var subtitle = obj.items[0].volumeInfo.subtitle;
-        var authors = obj.items[0].volumeInfo.authors[0];
-        var description = obj.items[0].volumeInfo.description;
-        var publisher = obj.items[0].volumeInfo.publisher;
-        var cover = obj.items[0].volumeInfo.imageLinks.smallThumbnail;
-        
-        document.getElementById("auto-title").innerHTML += title;
-        document.getElementById("auto-subtitle").innerHTML += subtitle;
-        document.getElementById("auto-author").innerHTML += authors;
-        document.getElementById("auto-description").innerHTML += description;
-        document.getElementById("auto-publisher").innerHTML += publisher;
-        document.getElementById("auto-cover").src = cover;
-      }    
+        var title_val = obj.items[0].volumeInfo.title;
+        var subtitle_val = obj.items[0].volumeInfo.subtitle;
+        var authors_val = obj.items[0].volumeInfo.authors[0];
+        var description_val = obj.items[0].volumeInfo.description;
+        var publisher_val = obj.items[0].volumeInfo.publisher;
+        var cover_val = obj.items[0].volumeInfo.imageLinks.smallThumbnail;
+				
+				title.value = title_val;
+        subtitle.value = subtitle_val;
+				author.value = authors_val;
+				description.value = description_val;
+				publisher.value = publisher_val;
+
+				if (title_val == null)
+					title.value = '';
+				if (subtitle_val == null)
+					subtitle.value = '';	 
+				if (authors_val == null)
+					authors.value = '';
+				if (description_val == null)
+					description.value = '';
+				if (publisher_val == null)
+					publisher.value = '';	
+			}
     };
 
     req.open("GET", req_url, false);
