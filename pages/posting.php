e6334eb87a862c1b503d202e89a84fe9ad89bff8
JustinO1055 <thejuiceman1038@gmail.com>
2020-11-27 20:04:25 -0600

    Added skeleton PHP code for posting upload into the DB. need to add the new fields, do validation and ensure that the upload will work correctly

--- pages/posting.php
+++ pages/posting.php
@@ -1,8 +1,8 @@
 <!-- 
   SAMPLE QUERIES
   ******************************************************************************************************************************************************
-  INSERT INTO Books(isbn_10, isbn_13, title, subtitle, publisher, description, edition, photo)
-  VALUES (1000000000, 1300000000001, "TestTitle", "TestSub", "pub", "test book", "8th", "../images/book_placeholder.jpg");
+  INSERT INTO Books(isbn_10, isbn_13, title, subtitle, publisher, description, photo)
+  VALUES (1000000000, 1300000000001, "TestTitle", "TestSub", "pub", "test book", "../images/book_placeholder.jpg");
   // will need to get these values from the api, initially do a search for the isbn, if nothing, insert into the DB, otherwise continue to listing
   // image here is the image obtained from the google API. can simply use the URL given
 
@@ -24,7 +24,140 @@
 
  -->
 <?php
-    session_start();
+  session_start();
+
+  /* //get the uid
+  $uid = $_SESSION["uid"];
+  if (isset($_POST["submitted"]) && $_POST["submitted"]) {
+    // get the values from the form
+    $isbn10 = trim($_POST["isbn10"]);
+    $isbn13 = trim($_POST["isbn13"]);
+    $title = trim($_POST["title"]);
+    $subtitle = trim($_POST["subtitle"]);
+    $authorList = trim($_POST["author"]);
+    $description = trim($_POST["description"]);
+    $publisher = trim($_POST["publisher"]);
+    $image = trim($_POST["image"]);
+    //will need to add the new fields added
+    
+    // change ' to &#039
+    $isbn10 = str_replace("'","&#039", $isbn10);
+    $isbn13 = str_replace("'","&#039", $isbn13);
+    $title = str_replace("'","&#039", $title);
+    $subtitle = str_replace("'","&#039", $subtitle);
+    $authors = str_replace("'","&#039", $authors);
+    $description = str_replace("'","&#039", $description);
+    $publisher = str_replace("'","&#039", $publisher);
+
+    // connect to DB and check connection
+    $db = new mysqli("localhost", "ottenbju", "Passw0rd", "ottenbju");
+    if ($db->connect_error)
+    {
+        die ("Connection failed: " . $db->connect_error);
+    }
+
+    // check to see if the book is in the DB
+    $q1 = "SELECT B.isbn_13 FROM Books B WHERE B.isbn_13 = '$isbn13' OR B.isbn_10 = '$isbn10'";
+    $r1 = $db->query($q1);
+
+    // if number of rows == 0 add book. otherwise go to adding the listing
+    if($r1->num_rows == 0) {
+
+      // add the book to the DB
+      $q2 = "INSERT INTO Books(isbn_10, isbn_13, title, subtitle, publisher, description,  photo) VALUES ('$isbn10', '$isbn13', '$title', '$subtitle', '$publisher', '$description', '$image')";
+      $r2 = $db->query($q2);
+
+      // split the authors up based off ", "
+      $authors = explode(", ", $authorList);
+      // go through the list of authors and seperate via first/ lastname and upload into DB
+      // prep query
+      $q3 = "INSERT INTO Authors(isbn_13, first_name, last_name) VALUES ";
+      foreach ($authors as $value){
+        // get first and last name
+        list($firstName, $lastName) = explode(" ", $value);
+        // append to query
+        $q3 .= "('$isbn13, $firstName, $lastName)";
+      }
+
+      // insert all authors into DB
+      $r3 = $db->query($q3);
+
+    }
+
+    // create query for listing, and insert into DB
+    $q4 = "INSERT INTO Listings (isbn_10, isbn_13, uid, book_condition, price, list_date, active) VALUES ('$isbn10', '$isbn13', '$uid', '$condition', '$price', curdate(), true)"; 
+    $r4 = $db->query($q4); 
+
+    //get the lid
+    $q5 = "SELECT L.lid FROM Listings L WHERE uid = '$uid' AND isbn_13 = '$isbn13' ORDER BY L.lid DESC";
+    $r5 = $db->query($q5); 
+    $lidQ = $r5->fetch_assoc();
+    $lid = $lidQ["lid"];
+
+    //prep query
+    $q6 = "INSERT INTO Images(image, lid) VALUES ";
+
+    // loop through all the images uploaded and upload them to the DB
+    $target_dir = "../avatar/";
+    $fileNames = array_filter($_FILES['fileToUpload']['name']); 
+    if(!empty($fileNames)){ 
+      foreach($_FILES['fileToUpload']['name'] as $key=>$val){ 
+
+        // value to give each image
+        $inc = 1;
+        
+        $fileName = basename($_FILES['fileToUpload']['name'][$key]); 
+        $targetFilePath = $targetDir . $fileName; 
+        $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType ) {    
+          // set error to this
+          $error =  "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+          $uploadOk = 0;
+        }        
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"][$key] > 500000)
+        {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0)
+        {
+            $error .= "Sorry, your file was not uploaded.";
+            
+        // if everything is ok, try to upload file    
+        }
+        else
+        {
+            //change the name of the file to the userID
+            $target_file = $target_dir . $lid . "_" . $inc . "." . $imageFileType;
+            // upload the file
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file));
+            
+            // append to query
+            $q6 .= "('$target_file', '$lid') ";
+        }
+        $inc++;
+      }
+      // input the images to the DB
+      $r6 = $db->query($q6);
+    }
+    // if there is an error. inform the user
+    else {
+        $error .= "Please select a file to upload";
+        $db->close();
+    }
+
+    // if querys work. send to the listing page
+    if ($r2 == true && $r3 == true && $r4 == true && $r6 === true)
+    {
+        header("Location: listing.php?lid=" . $lid);
+        $db->close();
+        exit();
+    }
+  } */
+
 ?>
 <!DOCTYPE html>
 <html>
