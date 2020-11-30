49bf8147624aa0087efbc5edf4faf462804a77db
patar221 <gohabsgok@gmail.com>
2020-11-29 21:43:35 -0600

    Changed CSS and forced users to be logged in

--- pages/posting.php
+++ pages/posting.php
@@ -1,3 +1,9 @@
+<?php 
+// start the php session
+  session_start();
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -20,7 +26,7 @@
     <body>
     <div class="topnav" id="pac-card">
             <a class="active" href="index.php">Home <i class="fa fa-fw fa-home"> </i></a>
-        <?php 
+            <?php 
             // if logged in
             if(isset($_SESSION["username"])) {
         ?>
@@ -31,15 +37,13 @@
             <a href="logout.php">LogOut <i class="fa fa-sign-out"></i></a></a>
 
         <?php
-            //if not logged in have links to sign up
+            //if not logged in direct user to logon page.
             } else {
 
-        ?>
-
-            <a href="signUp.php">SignUp <i class="fa fa-user-plus"> </i></a>
-            <a href="Login.php">LogIn <i class="fa fa-sign-in"></i></a>
-
-         <?php }?>
+                /* Redirect user to login page */
+                header("Location: Login.php"); 
+                exit();
+        }?>
 
             <div class="search-container">
                 <form action="search.php" method="get">
@@ -56,65 +60,92 @@
             </div>
         </div>    
     
-      <div class="type-selection">
-        <form id="create-posting">
-            <label>Manual</label>
-            <label class="switch">
-              <input id="toggle-switch" type="checkbox" checked>
-              <span class="slider round"></span>
-            </label>
-            <label>Automatic</label>
-          </div>
-				</form>
 					<form id="submit-form" class="input" action="submitposting.php" method="post" enctype="multipart/form-data">
-						<input type="hidden" name="submitted" value="1">
+            <input type="hidden" name="submitted" value="1">
             
-						<label>ISBN:</label>
-          	<input id="isbn" type="text" placeholder="ISBN.." name="isbn" />
-						<button id="auto-fill" type="button">Auto-Fill</button>
-            <label id="isbn_err" class="err_msg"></label><br />
+            <table class="formTable">
+              <tr>
+                <td><label>ISBN:</label></td>
+                <td><input id="isbn" type="text" placeholder="ISBN.." name="isbn" /></td>
+                <td><button id="auto-fill" type="button">Auto-Fill</button></td>
+                <label id="isbn_err" class="err_msg"></label></td>
+              </tr>
 
-            <input type="hidden" id="isbn-10" name="isbn-10">
-            <input type="hidden" id="isbn-13" name="isbn-13">
+              <input type="hidden" id="isbn-10" name="isbn-10">
+              <input type="hidden" id="isbn-13" name="isbn-13">
 
-						<label>Title:</label>
-						<input id="title" type="text" placeholder="Title.." name="title" readonly/>
-            <label id="title_err" class="err_msg"></label><br />
+              <tr>
+                <td><label>Title:</label></td>
+                <td><input id="title" type="text" placeholder="Title.." name="title" readonly/></td>
+                <td><label id="title_err" class="err_msg"></label></td>
+              </tr>
 
-						<label>Subtitle:</label>
-						<input id="subtitle" type="text" placeholder="Subtitle.." name="subtitle" readonly>
-            <label id="subtitle_err" class="err_msg"></label><br />
+              <tr>
+                <td><label>Subtitle:</label></td>
+                <td><input id="subtitle" type="text" placeholder="Subtitle.." name="subtitle" readonly></td>
+                <td><label id="subtitle_err" class="err_msg"></label></td>
+              </tr>
 
-						<label>Author:</label>
-						<input id="author" type="text" placeholder="Author.." name="author" readonly/>
-            <label id="author_err" class="err_msg"></label><br />
+              <tr>
+                <td><label>Author:</label></td>
+                <td><input id="author" type="text" placeholder="Author.." name="author" readonly/></td>
+                <td><label id="author_err" class="err_msg"></label></td>
+              </tr>
 
-						<label>Description:</label>
-						<input id="description" type="text" placeholder="Description.." name="description" readonly/>
-            <label id="description_err" class="err_msg"></label><br />
+              <tr>
+                <td><label>Description:</label></td>
+                <td><textarea rows="5" cols="69" id="description" type="text" placeholder="Description.." name="description" readonly></textarea></td>
+                <td><label id="description_err" class="err_msg"></label></td>
+              </tr>
 
-						<label>Publisher:</label>
-						<input id="publisher" type="text" placeholder="Publisher.." name="publisher" readonly/>
-            <label id="publisher_err" class="err_msg"></label><br />
+              <tr>
+                <td><label>Publisher:</label></td>
+                <td><input id="publisher" type="text" placeholder="Publisher.." name="publisher" readonly/></td>
+                <td><label id="publisher_err" class="err_msg"></label></td>
+              </tr>
 
-            <label>Price:</label>
-            <input id="price" type="number" placeholder="Price.." name="price" step="0.01" min="0"/>
-            <label id="price_err" class="err_msg"></label><br />
+              <tr>
+                <td><label>Price:</label></td>
+                <td><input id="price" type="number" placeholder="Price.." name="price" step="0.01" min="0"/></td>
+                <td><label id="price_err" class="err_msg"></label></td>
+              </tr>
 
-            <label>Condition:</label>
-            <select id="condition" name="condition">
-              <option value="good">Good</option>
-              <option value="fair">Fair</option>
-              <option value="poor">Poor</option>
-            </select><br />
+              <tr>
+                <td><label>Condition:</label></td>
+                <td><select id="condition" name="condition">
+                  <option value="excellent">Excellent</option>
+                  <option value="good">Good</option>
+                  <option value="fair">Fair</option>
+                  <option value="poor">Poor</option>
+                </select></td>
+              </tr>
 
-						<label>Upload Images:</label>
-            <input type="file" name="files[]" multiple ><br />
+              <tr>
+                <td><label>Upload Images:</label></td>
+                <td><input type="file" name="files[]" multiple ></td>
+              </tr>
 
-						<img id="cover" src=""></img><br />
-            <input type="hidden" id="cover-link" name="cover-link">
-            <button id="submit-posting" type="submit">Submit</button>
-					</form>
+              <img id="cover" src=""></img><br />
+              <input type="hidden" id="cover-link" name="cover-link">
+
+              <tr>
+                <td></td><td><button id="submit-posting" type="submit">Submit</button></td>
+              </tr>
+
+            </table>
+          </form>
+          
+          <div class="type-selection">
+            <form id="create-posting">
+              <label>Manual</label>
+              <label class="switch">
+                <input id="toggle-switch" type="checkbox" checked>
+                <span class="slider round"></span>
+              </label>
+              <label>Automatic</label>
+            </form>
+          </div>
+
       </div>
     <script type="text/javascript" src="../js/JavaScript.js"></script>
     <script type="text/javascript" src="../js/googlebooksapi.js"></script>
